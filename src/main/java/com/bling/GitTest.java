fc9524218d474690722c8e57ccf22f57bd18427e
JL <1164628957@qq.com>
2024-04-06 16:16:50 +0800

    push commit2

--- src/main/java/com/bling/GitTest.java
+++ src/main/java/com/bling/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello hotfix1");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
